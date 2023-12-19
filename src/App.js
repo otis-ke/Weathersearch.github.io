4d0e056bd5fcadbaa369d72c04b81f3d15d70f36
otis-ke <vincentotiskisia@gmail.com>
2023-12-19 04:01:41 +0300

    communual

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import './index.css'
-import React from 'react';
 import './App.css';
 import WeatherApp from './components/WeatherApp/WeatherApp';
+import './index.css'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>NOTIS WEATHER</h1>
-      </header>
-
-      <main>
-        <WeatherApp />
-      </main>
-
-      <footer>
-        <p>&copy; 2023 Notis Weather. All rights reserved.</p>
-      </footer>
+    <WeatherApp/>
     </div>
-  );
+  ); 
 }
 
 export default App;
