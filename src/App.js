59fb2bd815c51d1c27c155d17d4186e618bdd218
otis-ke <vincentotiskisia@gmail.com>
2023-12-19 03:47:14 +0300

    i dont know

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import './index.css'
+import React from 'react';
 import './App.css';
 import WeatherApp from './components/WeatherApp/WeatherApp';
-
 
 function App() {
   return (
     <div className="App">
-    <WeatherApp/>
+      <header className="App-header">
+        <h1>NOTIS WEATHER</h1>
+      </header>
+
+      <main>
+        <WeatherApp />
+      </main>
+
+      <footer>
+        <p>&copy; 2023 Notis Weather. All rights reserved.</p>
+      </footer>
     </div>
-  ); 
+  );
 }
 
 export default App;
